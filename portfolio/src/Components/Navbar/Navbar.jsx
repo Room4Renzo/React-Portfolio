4716b5ea1b74caddd49cb4e2c27969ff8b1532f6
lorenzo.afw@gmail.com <lorenzo.afw@gmail.com>
2023-08-27 15:55:31 +0100

    Added Logic to display basic Navbar

--- portfolio/src/Components/Navbar/Navbar.jsx
+++ portfolio/src/Components/Navbar/Navbar.jsx
@@ -3,7 +3,25 @@ import "./Navbar.css";
 function Navbar() {
 	return (
 		<div className="Navbar">
-			<h1>My Portfolio</h1>
+			<h1>Lorenzo Francis-Walker</h1>
+			<h3>Software Developer</h3>
+
+			<div className="Navbar-Links">
+				<ul>
+					<li>
+						<a href="#About">About</a>
+					</li>
+					<li>
+						<a href="#Projects">Projects</a>
+					</li>
+					<li>
+						<a href="#Skills">Skills</a>
+					</li>
+					<li>
+						<a href="#Contact">Contact</a>
+					</li>
+				</ul>
+			</div>
 		</div>
 	);
 }
