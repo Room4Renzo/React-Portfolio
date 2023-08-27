63f806b93cd705c1690d50faa0edb3cc6bf5cfdc
lorenzo.afw@gmail.com <lorenzo.afw@gmail.com>
2023-08-27 16:11:57 +0100

    Added information and structure for About section

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
-import Navbar from "./Components/Navbar/Navbar";
+import Navbar from "./Components/Navbar/Navbar.jsx";
+import About from "./Components/About/About.jsx";
 
 function App() {
 	return (
 		<div className="App">
 			<Navbar />
+			<About />
 		</div>
 	);
 }
--- portfolio/src/Components/About/About.jsx
+++ portfolio/src/Components/About/About.jsx
@@ -1,9 +1,27 @@
 import "./About.css";
+import Smile from "../../images/SmileSmall.png";
 
 function About() {
 	return (
 		<div className="About">
-			<h1>About</h1>
+			<div id="headshot">
+				<img src={Smile} />
+				<h2>About Me:</h2>
+			</div>
+			<div id="aboutMe">
+				<p>
+					My love for computers comes from a fairly young age and is greater
+					than my current experience. I was gifted a PC that soon stopped
+					working. I found some instructions online suggesting how I could fix
+					it. I used a second PC that also didn't work to make one working unit.
+					My Frankenstein. I had always been interested in technology but this
+					is where my love for understanding computers and how they work. Once
+					you learn about different types of hardware theres not many places to
+					go. ie networking, coding, etc. Fortunately for me, the width and
+					breadth of options are vast, so there is alot left for me to learn and
+					I never want my journey to end.
+				</p>
+			</div>
 		</div>
 	);
 }
