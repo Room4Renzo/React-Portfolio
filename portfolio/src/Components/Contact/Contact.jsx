bfcdc9b7d5bc5e4ba89c1aad0d9c8e7dd1fb31a0
lorenzo.afw@gmail.com <lorenzo.afw@gmail.com>
2023-08-27 19:05:30 +0100

    Added basic contact information and links

--- portfolio/src/Components/Contact/Contact.jsx
+++ portfolio/src/Components/Contact/Contact.jsx
@@ -3,7 +3,26 @@ import "./Contact.css";
 function Contact() {
 	return (
 		<div className="Contact">
-			<h1>Contact</h1>
+			<h3>Contact</h3>
+
+			<p>
+				GitHub:
+				<a href="https://github.com/Room4Renzo">
+					<img src="https://img.icons8.com/ios-filled/50/000000/email.png" />
+				</a>
+			</p>
+			<p>
+				LinkedIn:
+				<a href="https://Linkedin.com">
+					<img src="https://img.icons8.com/ios-filled/50/000000/email.png" />
+				</a>
+			</p>
+			<p>
+				Email:
+				<a href="mailto:Lorenzo.afw@gmail.com">
+					<img src="https://img.icons8.com/ios-filled/50/000000/email.png" />
+				</a>
+			</p>
 		</div>
 	);
 }
